4752e02d7388b472330eef7a98fa5e102d06529e
Carlos Polanco <carlospolanco@Carloss-Mac-mini.local>
2021-05-21 16:11:59 -0400

    adding dropdown

--- src/index.js
+++ src/index.js
@@ -1,13 +1,39 @@
-
-
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-  function grabTask(e) {
-    e.preventDefault()
-    let inputVal = document.querySelector('#new-task-description')
-    addTask(inputVal.value)
-    inputVal.value = ''
+
+  function addDropdown () {
+    let dropdown = document.createElement('select')
+    dropdown.innerHTML = `
+    <option value="red">High priority</option>
+    <option value="yellow">Mediun</option>
+    <option value="green">Low</option>
+  
+`
+
+let textBox = document.querySelector('#new-task-description')
+textBox.insertAdjacentElement('afterend', dropdown)
+
+
   }
-  let form = document.querySelector('#create-task-form')
-  form.addEventListener('submit', grabTask)
+  // your code here
+  function addTask(task) {
+    let li = document.createElement("li");
+    li.textContent = task;
+    let done = document.createElement("button");
+    done.textContent = "done"
+    done.addEventListener('click', ()=> li.remove())
+    li.append(done);
+    document.querySelector("#tasks").append(li);
+  }
+
+  function grabTask(e) {
+    e.preventDefault();
+    let inputVal = document.querySelector("#new-task-description");
+    addTask(inputVal.value);
+    inputVal.value = "";
+  }
+  let form = document.querySelector("#create-task-form");
+  addDropdown()
+  form.addEventListener("submit", grabTask);
 });
+
+
