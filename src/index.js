d712bed9a31bb52ad36a24621e9ab9f1466b8797
github username <github email address>
2021-05-21 16:56:08 -0400

    added sorter of priorities

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
+const taskArray = []
+
+
+
 document.addEventListener("DOMContentLoaded", () => {
 
   function addDropdown () {
     let dropdown = document.createElement('select')
     dropdown.innerHTML = `
     <option value="red">High priority</option>
-    <option value="yellow">Mediun</option>
-    <option value="green">Low</option>
-  
-`
-
-let textBox = document.querySelector('#new-task-description')
-textBox.insertAdjacentElement('afterend', dropdown)
-
-
+    <option value="yellow">Medium priority</option>
+    <option value="green">Low priority</option>
+    `
+    dropdown.id = 'dropdown'
+    let textBox = document.querySelector('#new-task-description')
+    textBox.insertAdjacentElement('afterend', dropdown)
   }
+
   // your code here
   function addTask(task) {
     let li = document.createElement("li");
@@ -22,7 +24,37 @@ textBox.insertAdjacentElement('afterend', dropdown)
     done.textContent = "done"
     done.addEventListener('click', ()=> li.remove())
     li.append(done);
-    document.querySelector("#tasks").append(li);
+    let color = document.querySelector('#dropdown').value
+    li.style.color = color
+    li.className = color
+    console.log(li)
+    //document.querySelector("#tasks").append(li);
+    //sorted append
+    sortedAppend(li)
+  }
+
+  function sortedAppend(task) {
+    switch (task.style.color) {
+      case 'red':
+        document.querySelector("#tasks").insertAdjacentElement('afterbegin', task)
+        break;
+      case 'yellow':
+        let greenTask = document.querySelector('.green')
+        console.log(greenTask)
+        if(greenTask) {
+          greenTask.insertAdjacentElement('beforebegin', task)
+        } else {
+          document.querySelector("#tasks").append(task)
+        }
+        break;
+      case 'green':
+        document.querySelector("#tasks").append(task)
+        break;
+      
+      default:
+        break;
+    }
+
   }
 
   function grabTask(e) {
