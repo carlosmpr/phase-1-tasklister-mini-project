5dbdb21fdb9d5ed9b1d38c992d1ee4300f19b79d
github username <github email address>
2021-05-21 15:26:41 -0400

    Made grab task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  function grabTask(e) {
+    e.preventDefault()
+    let inputVal = document.querySelector('#new-task-description')
+    addTask(inputVal.value)
+    inputVal.value = ''
+  }
+  let form = document.querySelector('#create-task-form')
+  form.addEventListener('submit', grabTask)
 });
